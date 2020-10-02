f3bbe766ec3dfea4e5e347039a6ed218da7b5051
special1024 <394585940@qq.com>
2020-10-02 20:08:15 +0800

    提交代码

---
+++ testthread/philosopher/Chopstick.java
@@ -0,0 +1,18 @@
+package testthread.philosopher;
+
+public class Chopstick {
+
+    private int num;
+
+    public Chopstick(int num){
+        this.num = num;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
