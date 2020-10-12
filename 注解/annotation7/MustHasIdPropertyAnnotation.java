d129b3cd6ed8e64d4c33cca93036f78a14b3efc7
special1024 <394585940@qq.com>
2020-10-12 22:44:51 +0800

    提交代码

---
+++ 注解/annotation7/MustHasIdPropertyAnnotation.java
@@ -0,0 +1,14 @@
+package 注解.annotation7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//表示这个注解只能出现在类上面
+@Target(ElementType.TYPE)
+//表示该注解可以被反射机制读取到
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MustHasIdPropertyAnnotation {
+
+}
---
+++ 注解/annotation7/User.java
@@ -0,0 +1,9 @@
+package 注解.annotation7;
+
+//@Id
+@MustHasIdPropertyAnnotation
+public class User {
+
+    int id;
+    String name;
+}
