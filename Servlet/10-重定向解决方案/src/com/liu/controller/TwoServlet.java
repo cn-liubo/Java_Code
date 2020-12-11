6277531fa0855e1d39da56e45c9c81b7d964adb0
“special1024 <394585940@qq.com>
2020-12-11 13:05:57 +0800

    提交

---
+++ Servlet/10-重定向解决方案/src/com/liu/controller/TwoServlet.java
@@ -0,0 +1,16 @@
+package com.liu.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TwoServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("TwoServlet负责韭菜炒鸡蛋");
+
+        //
+    }
+}
---
+++ Servlet/10-重定向解决方案/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 39458
+  Date: 2020/11/4
+  Time: 8:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
