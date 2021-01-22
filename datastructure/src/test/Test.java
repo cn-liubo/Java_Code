701fe6b541d093c043e786f174cbf82ed38afd46
“special1024 <394585940@qq.com>
2021-01-22 18:50:05 +0800

    test

---
+++ datastructure/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 100;
+        Integer b = new Integer(a);
+        System.out.println(b);
+
+        a = 200;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
