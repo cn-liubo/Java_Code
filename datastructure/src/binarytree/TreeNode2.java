547b18462715981593af523dcdc1cc367d44eba2
“special1024 <394585940@qq.com>
2021-01-24 19:55:25 +0800

    node

---
+++ datastructure/src/binarytree/TreeNode2.java
@@ -0,0 +1,16 @@
+package binarytree;
+
+public class TreeNode2 {
+    public char val;
+    public TreeNode2 left;
+    public TreeNode2 right;
+
+    public TreeNode2(char val) {
+        this.val = val;
+    }
+
+//    @Override
+//    public String toString() {
+//        return String.format("%c", val);
+//    }
+}
