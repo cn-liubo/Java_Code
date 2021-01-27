4fafc3d0571e5fe8a7f3018de94af65b55ccd864
“special1024 <394585940@qq.com>
2021-01-27 18:20:28 +0800

    treenode

---
+++ datastructure/src/binarytree/leetcode/TreeNode.java
@@ -0,0 +1,13 @@
+package binarytree.leetcode;
+
+public class TreeNode {
+
+    int val;
+    binarytree.leetcode.TreeNode left;
+    binarytree.leetcode.TreeNode right;
+
+    TreeNode (int val) {
+        this.val = val;
+    }
+
+}
