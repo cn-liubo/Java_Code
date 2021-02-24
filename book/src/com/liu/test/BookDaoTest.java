211647a694af1c94f4d6f24a33c3c5d38915e470
special1024 <394585940@qq.com>
2021-02-24 21:25:13 +0800

    BookDaoTest

---
+++ book/src/com/liu/test/BookDaoTest.java
@@ -0,0 +1,68 @@
+package com.liu.test;
+
+import com.liu.dao.BookDao;
+import com.liu.dao.impl.BookDaoImpl;
+import com.liu.pojo.Book;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class BookDaoTest {
+
+    private BookDao bookDao = new BookDaoImpl();
+
+    @Test
+    public void addBook() {
+        bookDao.addBook(new Book(null, "葵花宝典", "曹公公", new BigDecimal(999), 10000, 0, null));
+    }
+
+    @Test
+    public void deleteBookById() {
+        bookDao.deleteBookById(21);
+    }
+
+    @Test
+    public void updateBook() {
+        bookDao.updateBook(new Book(21, "葵花宝典", "王公公", new BigDecimal(999), 1000000, 0, null));
+    }
+
+    @Test
+    public void queryBookById() {
+        System.out.println(bookDao.queryBookById(21));
+    }
+
+    @Test
+    public void queryBooks() {
+        for(Book book : bookDao.queryBooks()) {
+            System.out.println(book);
+        }
+    }
+
+    @Test
+    public void queryForPageTotalCount() {
+        System.out.println(bookDao.queryForPageTotalCount());
+    }
+
+    @Test
+    public void queryForPageItems() {
+        for (Book book : bookDao.queryForPageItems(8, 4)) {
+            System.out.println(book);
+        }
+    }
+
+    @Test
+    public void queryForPageTotalCountByPrice() {
+        System.out.println(bookDao.queryForPageTotalCountByPrice(10, 50));
+//        Assert.assertNotNull(bookDao.queryForPageTotalCountByPrice(10, 50));
+    }
+
+    @Test
+    public void queryForPageItemsByPrice() {
+        for (Book book : bookDao.queryForPageItemsByPrice(0, 4, 10, 50)) {
+            System.out.println(book);
+        }
+    }
+}
