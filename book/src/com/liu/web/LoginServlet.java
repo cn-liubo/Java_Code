ab4cd64e9011477350a895a60eeab3df91273a8e
special1024 <394585940@qq.com>
2021-03-02 20:09:42 +0800

    LoginServlet

---
+++ book/src/com/liu/web/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.liu.web;
+
+import com.liu.pojo.User;
+import com.liu.service.UserService;
+import com.liu.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet(name = "LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+}
