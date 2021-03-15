faaf2f7e1b322c9d09e9f58779ff1df0110cc769
special1024 <394585940@qq.com>
2021-03-15 22:30:37 +0800

    OrderDao.java

---
+++ mybatis/ch06-dynamic-day/src/main/java/com/liu/dao/OrderDao.java
@@ -0,0 +1,20 @@
+package com.liu.dao;
+
+import com.liu.domain.Student;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    //动态sql，使用java对象作为参数
+    List<Student> selectStudentIf(Student student);
+
+    //where的使用
+    List<Student> selectStudentWhere(Student student);
+
+    //foreach的使用
+    //用法1：
+    List<Student> selectForEachOne(List<Integer> idList);
+    //用法2：
+    List<Student> selectForEachTwo(List<Student> idList);
+}
