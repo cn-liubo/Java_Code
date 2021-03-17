f8b87dd9454379bdae6014edbd45c490c2398fb7
special1024 <394585940@qq.com>
2021-03-17 22:31:50 +0800

    QueryParam

---
+++ mybatis/ch06-dynamic-day/src/main/java/com/liu/vo/QueryParam.java
@@ -0,0 +1,31 @@
+package com.liu.vo;
+
+public class QueryParam {
+
+    private String paramName;
+    private Integer paramAge;
+
+    public QueryParam() {
+    }
+
+    public QueryParam(String paramName, Integer paramAge) {
+        this.paramName = paramName;
+        this.paramAge = paramAge;
+    }
+
+    public String getParamName() {
+        return paramName;
+    }
+
+    public void setParamName(String paramName) {
+        this.paramName = paramName;
+    }
+
+    public Integer getParamAge() {
+        return paramAge;
+    }
+
+    public void setParamAge(Integer paramAge) {
+        this.paramAge = paramAge;
+    }
+}
