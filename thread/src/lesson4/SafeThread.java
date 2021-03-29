c48cf54306a74d440063dc071346b31e4a677469
special1024 <394585940@qq.com>
2021-03-29 21:18:58 +0800

    SafeThread

---
+++ thread/src/lesson4/SafeThread.java
@@ -0,0 +1,38 @@
+package lesson4;
+
+public class SafeThread {
+
+    private static int COUNT = 0;
+
+    //有一个变量COUNT=0，同时启动20个线程，每个线程循环1000次，每次循环COUNT++
+    //等着20个子线程执行完毕之后，在main线程打印COUNT（预期 COUNT=20000）
+    public static void main(String[] args) throws InterruptedException {
+
+        Class clazz = SafeThread.class;
+
+        //尽量同时启动，不让new Thread耗时影响
+        Thread[] threads = new Thread[20];
+        for(int i = 0; i < 20; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int j = 0; j < 1000; j++) {
+                        synchronized (clazz) {
+                            COUNT++;
+                        }
+                    }
+                }
+            });
+        }
+        for(Thread thread : threads) {
+            thread.start();
+        }
+
+        //让main线程阻塞等待所有的20个子线程执行完毕
+        for(Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println(COUNT);
+    }
+}
