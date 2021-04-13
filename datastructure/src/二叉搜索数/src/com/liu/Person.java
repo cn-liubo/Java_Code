a308a27a3a1fbb0806632a5df07abff18257cf19
special1024 <394585940@qq.com>
2021-04-13 22:28:36 +0800

    Person

---
+++ datastructure/src/二叉搜索数/src/com/liu/Person.java
@@ -0,0 +1,29 @@
+package com.liu;
+
+public class Person implements Comparable<Person>{
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return age - o.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                '}';
+    }
+}
