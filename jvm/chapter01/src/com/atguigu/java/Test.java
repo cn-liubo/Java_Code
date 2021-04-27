636983478bfda2ece5806ffb80d52aba93856657
special1024 <394585940@qq.com>
2021-04-27 20:48:44 +0800

    Test

---
+++ jvm/chapter01/src/com/atguigu/java/Test.java
@@ -0,0 +1,14 @@
+package com.atguigu.java;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
