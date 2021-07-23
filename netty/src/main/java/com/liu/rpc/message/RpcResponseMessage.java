0ebe23f0f175be6e77bcc602a79399d30b0da378
special1024 <394585940@qq.com>
2021-07-23 10:52:53 +0800

    RpcResponseMessage.java

---
+++ netty/src/main/java/com/liu/rpc/message/RpcResponseMessage.java
@@ -0,0 +1,26 @@
+package com.liu.rpc.message;
+
+import com.liu.message.Message;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author yihang
+ */
+@Data
+@ToString(callSuper = true)
+public class RpcResponseMessage extends Message {
+    /**
+     * 返回值
+     */
+    private Object returnValue;
+    /**
+     * 异常值
+     */
+    private Exception exceptionValue;
+
+    @Override
+    public int getMessageType() {
+        return RPC_MESSAGE_TYPE_RESPONSE;
+    }
+}
