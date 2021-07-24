481d8e11329aa30b40b8201e7f810576c7f773ea
special1024 <394585940@qq.com>
2021-07-24 14:03:42 +0800

    SequenceIdGenerator.java

---
+++ netty/src/main/java/com/liu/rpc/protocol/SequenceIdGenerator.java
@@ -0,0 +1,11 @@
+package com.liu.rpc.protocol;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequenceIdGenerator {
+    private static final AtomicInteger id = new AtomicInteger();
+
+    public static int nextId() {
+        return id.incrementAndGet();
+    }
+}
